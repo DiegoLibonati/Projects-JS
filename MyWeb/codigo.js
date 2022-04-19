662860e171de9bc3c6e0bbfb9be71fa9bf802c3b
DiegoLibonati <diego.libonati1998@gmail.com>
2022-04-18 22:13:56 -0300

    update myweb

--- MyWeb/codigo.js
+++ MyWeb/codigo.js
@@ -1,5 +1,6 @@
 const btnsNav = document.querySelectorAll(".div_center_logo i");
 const shownav = document.querySelector(".div_center_nav");
+const btnShowInfo = document.querySelector(".div_info button");
 
 const colors = "ABCDEF1234567890";
 const queryMatch = window.matchMedia("(min-width: 768px)");
@@ -13,8 +14,10 @@ let intervalRNO;
 let intervalHeaderResponsive;
 let arrayResponsive = [];
 
+let sectionFirstShowInfo = false;
 
-// Responsive config
+
+// Header / Nav Config
 btnsNav.forEach(function(btn){
 
     btn.addEventListener("click", (e)=>{
@@ -142,3 +145,101 @@ function rainbowResponsive(){
 }
 
 intervalHeaderFullScreen = setInterval(raninbowHeaderFullScreen, 1000);
+
+
+// Firts Section
+
+btnShowInfo.addEventListener("click", showInfo);
+
+function showInfo(){
+    
+    if (queryMatch.matches){
+        if (sectionFirstShowInfo == true){
+            document.querySelector(".div_info p").style.height = "0";
+            document.querySelector(".div_info p").style.width = "0";
+            document.querySelector(".div_info p").style.opacity = "0";
+            document.querySelector(".div_info p").style.padding = "0";
+            document.querySelector(".div_info button i").setAttribute("class", "fa-solid fa-angle-right");
+    
+            sectionFirstShowInfo = false;
+        } else {
+            document.querySelector(".div_info p").style.height = "auto";
+            document.querySelector(".div_info p").style.opacity = "1";
+            document.querySelector(".div_info p").style.padding = "10px";
+            document.querySelector(".div_info p").style.width = "auto";
+            document.querySelector(".div_info button i").setAttribute("class", "fa-solid fa-angle-left");
+    
+            sectionFirstShowInfo = true;
+        }
+    } else {
+        if (sectionFirstShowInfo == true){
+            document.querySelector(".div_info p").style.height = "0";
+            document.querySelector(".div_info p").style.width = "0";
+            document.querySelector(".div_info p").style.opacity = "0";
+            document.querySelector(".div_info p").style.padding = "0";
+            document.querySelector(".div_info button i").setAttribute("class", "fa-solid fa-angle-down");
+    
+            sectionFirstShowInfo = false;
+        } else {
+            document.querySelector(".div_info p").style.height = "auto";
+            document.querySelector(".div_info p").style.opacity = "1";
+            document.querySelector(".div_info p").style.padding = "10px";
+            document.querySelector(".div_info p").style.width = "auto";
+            document.querySelector(".div_info button i").setAttribute("class", "fa-solid fa-angle-up");
+    
+            sectionFirstShowInfo = true;
+        }
+    }
+
+}
+
+// Document Loaded
+
+document.addEventListener("DOMContentLoaded", (event)=>{
+
+    if(queryMatch.matches){
+        document.querySelector(".div_info button i").setAttribute("class", "fa-solid fa-angle-right");
+    } else {
+        document.querySelector(".div_info button i").setAttribute("class", "fa-solid fa-angle-down");
+    }
+
+
+  const dataText = ["WELCOME TO MY WORLD"];
+  
+  function typeWriter(text, i, fnCallback) {
+
+    if (i < (text.length)) {
+
+     document.querySelector(".div_title h1").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
+
+
+      setTimeout(function() {
+        typeWriter(text, i + 1, fnCallback)
+      }, 100);
+    }
+
+    else if (typeof fnCallback == 'function') {
+      // call callback after timeout
+      setTimeout(fnCallback, 700);
+    }
+  }
+
+   function StartTextAnimation(i) {
+     if (typeof dataText[i] == 'undefined'){
+        setTimeout(function() {
+          StartTextAnimation(0);
+        }, 20000);
+     }
+
+    if (i < dataText[i].length) {
+
+     typeWriter(dataText[i], 0, function(){
+
+       StartTextAnimation(i + 1);
+     });
+    }
+  }
+
+  StartTextAnimation(0);
+
+});
