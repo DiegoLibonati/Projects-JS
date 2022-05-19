78dfb9a6b6e9bbf106670ae0a2da58b8a3d3d4f6
DiegoLibonati <diego.libonati1998@gmail.com>
2022-05-19 14:01:27 -0300

    update simon says

--- Games/Simon say/codigo.js
+++ Games/Simon say/codigo.js
@@ -1,23 +1,40 @@
 const colorsBox = document.querySelectorAll(".box_color");
+const btnStart = document.querySelector(".section_start_row1 button");
+const whoPlaysHtml = document.querySelector(".whoPlays");
+const scoreHtml = document.querySelector(".score");
+const btnsDifficulty = document.querySelectorAll(".btnDif"); 
 
 let valuesCanPlayIA = ["green", "yellow", "red", "blue"];
 let arrayIA = [];
-let iaCanPlay = true;
+let iaCanPlay = false;
 let iaCount = 0;
 
 let userCanPlay = false; 
 let arrayUser = [];
 
-let loadColorsBox = false
-
 let checkIfUserIsEqualToIa = false;
 
+let score = 0;
+
+// Function: Inicio de juego
+const startGame = () =>{
+
+    iaCanPlay = true;
+
+    whoPlays();
+
+}
+
+// Btn Start Game
+btnStart.addEventListener("click", startGame);
+
+// Si userCanPlay es True, al clickear vas a cambiar el color de la boxColor y vas a pushear el elemento al arrayUser. Si el array del usuario es igual al array previo de la IA. Pasa a chequear ambos arrays.
 const userSystemPlay = (e) => {
 
     if (userCanPlay == true){
         let itemId = e.currentTarget.id;
         
-        changeColorViewOfTabletop(itemId, e.currentTarget);
+        changeColorViewOfTabletop(itemId, e.currentTarget, 100);
     
         arrayUser.push(itemId);   
     
@@ -30,25 +47,27 @@ const userSystemPlay = (e) => {
 
 }
 
+// Obtenes los colores y le damos a cada uno la funcion de click.
 colorsBox.forEach(function(colorBox){
 
     colorBox.addEventListener("click", userSystemPlay);
 
 });
 
+// Funcion que cambia los colores de sus respectivos boxes.
+const changeColorViewOfTabletop = (itemId, item, time) => {
 
-const changeColorViewOfTabletop = (itemId, item) => {
-        
     item.classList.add(`${itemId}-color`);
 
     setTimeout(()=>{
-
+    
         item.classList.remove(`${itemId}-color`);
-
-    },500);
+    
+    },time); 
 
 }
 
+// Funcion que permite obtener algun color aleatoria de valuesCanPlayIA y los retorna.
 const iaSelectColorToPlay = () => {
 
     const randomColor = Math.floor(Math.random() * valuesCanPlayIA.length);
@@ -57,71 +76,78 @@ const iaSelectColorToPlay = () => {
 
 }
 
+// Funcion que permite hacer jugar a la maquina. Si el array de la maquina es menor a iaCount va a seguir jugando. Va elegir un color random y lo va pushear al arrayIA existente. 
+// Luego, a traves de la funcion loopIa va a recorrer el arrayIA y va a mostrar que colores se estan tocando en pantalla si el id de la caja y el valor del array son iguales.
+// Esto tendra un delay de 1000 segundos y volvera a llamar a la funcion.
 const iaPlay = () => {
 
     if (!(arrayIA.length === iaCount)){
+
         const iaPickColor = iaSelectColorToPlay();
         
         arrayIA.push(iaPickColor);
-        console.log(arrayIA)
 
-        for(let i = 0; i < colorsBox.length; i++){
-
-            for (let y = 0; y < arrayIA.length; y++){
-
-                if (colorsBox[i].id == arrayIA[y]){
-
-                    setTimeout(()=>{
-
-                        changeColorViewOfTabletop(colorsBox[i].id ,colorsBox[i]);
-    
-                    }, 1000 * y + 1);
-
-                }
-
-            }    
-
+        const delay = (amount = number) => {
+            return new Promise((resolve) => {
+                setTimeout(resolve, amount);
+            });
         }
+            
+        async function loopIa() {
+            for (let i = 0; i < arrayIA.length; i++) {
+                
+                colorsBox.forEach(function(colorBox){
 
+                    if (colorBox.id == arrayIA[i]){
+                        changeColorViewOfTabletop(arrayIA[i], colorBox, 500);
+                    }
+
+                })
+
+                await delay(1000);
+            }
+            }
+            loopIa();
     }
-
 }
-  
 
+// Funcion que dictamina quien juega.
 const whoPlays = () => {
 
+    // Juega la maquina
     if (iaCanPlay == true && checkIfUserIsEqualToIa == false && userCanPlay == false){
-
+        whoPlaysHtml.innerHTML = `IA PLAYS`;
         iaCount++
 
-        for (let i = 0; i < iaCount; i++){
-
-          setTimeout(iaPlay,1000 * i + 1);
-
-        }
+        iaPlay()
 
         iaCanPlay = false;
 
         setTimeout(() => {
             userCanPlay = true
+            console.log(userCanPlay)
             whoPlays();
-        }, 1010 * iaCount + 1)
+        }, 1000 * iaCount)
     }
 
+    // Juega el usuario
     if (userCanPlay == true && checkIfUserIsEqualToIa == false && iaCanPlay == false){
+        whoPlaysHtml.innerHTML = `ITS YOUR TURN!`;
         arrayUser = [];
     }
 
+    // Se fija si los arrays son iguales
     if (checkIfUserIsEqualToIa == true && userCanPlay == false && iaCanPlay == false){
        let result = checkArraysIaAndUser(arrayIA, arrayUser);
 
        if (result == true){
-           setTimeout(() => {
-            console.log("sigue")
-            iaCanPlay = true;
-            checkIfUserIsEqualToIa = false;
-            whoPlays();
-        }, 1010 * iaCount + 1)
+            setTimeout(() => {
+                score++
+                scoreHtml.innerHTML = `SCORE: ${score}`;
+                iaCanPlay = true;
+                checkIfUserIsEqualToIa = false;
+                whoPlays();
+            }, 500);
        } else {
            console.log("PERDISTE");
        }
@@ -129,6 +155,7 @@ const whoPlays = () => {
 
 }
 
+// Funcion que permite comparar amabos arrays.
 const checkArraysIaAndUser = (arr, arr2) => {
 
         if (arr === arr2) return true;
@@ -143,4 +170,4 @@ const checkArraysIaAndUser = (arr, arr2) => {
     
 }
 
-whoPlays();
+
