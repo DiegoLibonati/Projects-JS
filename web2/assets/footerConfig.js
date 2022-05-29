c4e8683f067ef6442d059ca7a9fc3dc6a1b1db7f
DiegoLibonati <diego.libonati1998@gmail.com>
2022-05-29 16:24:01 -0300

    update

--- web2/assets/footerConfig.js
+++
@@ -1,7 +0,0 @@
-const ageOfHtml = document.querySelector(".footer_container h2 span");
-
-const currentDate = new Date();
-
-const currentDateAge = currentDate.getFullYear();
-
-ageOfHtml.textContent = `${currentDateAge}`;
